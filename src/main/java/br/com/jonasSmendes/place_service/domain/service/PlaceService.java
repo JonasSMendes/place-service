a1136a9a93860734b7b5fe127ed939d76d023916
jonas mendes <jonas.jsm2903@gmail.com>
2024-12-02 17:21:59 -0300

    adicionando controller e service

--- src/main/java/br/com/jonasSmendes/place_service/domain/service/PlaceService.java
+++ src/main/java/br/com/jonasSmendes/place_service/domain/service/PlaceService.java
@@ -1,4 +1,19 @@
 package br.com.jonasSmendes.place_service.domain.service;
 
+import br.com.jonasSmendes.place_service.domain.Place;
+import br.com.jonasSmendes.place_service.domain.repository.PlaceRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
 public class PlaceService {
+    private PlaceRepository placeRepository;
+
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    public Mono<Place> create (Place place){
+        return placeRepository.save(place);
+    }
 }
